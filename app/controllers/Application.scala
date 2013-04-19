5ac1db772fc149c83c9f922090b9fd4b2c713592
Sander Mak <sam@solcon.nl>
2013-04-19 22:09:02 +0200

    Move GA properties out of HTML into application.conf

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,9 +3,15 @@ package controllers
 import play.api.libs.concurrent.Execution.Implicits._
 import play.api.mvc._
 import service.ScalabitzService
+import play.api.Play.current
+import play.api.Play
 
 
 object Application extends Controller {
+
+  // Google Analytics properties, used in index.scala.html
+  lazy val domain = Play.configuration.getString("site.ga.domain").get
+  lazy val trackingid = Play.configuration.getString("site.ga.trackingid").get
 
   def index = Action {
     Async {
